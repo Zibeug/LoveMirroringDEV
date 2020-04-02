e52034e51778e565d0bce3c13a2306e2b3ab2d4d
Sébastien Berger <berger.sebastien@bluewin.ch>
2020-04-02 14:03:55 +0200

    Testunitaire

---
+++ LoveMirorring/TestUnitaireLoveMirroring/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestUnitaireLoveMirroring
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

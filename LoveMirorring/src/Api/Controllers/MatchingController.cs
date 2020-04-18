649f79dacbe53d23e77c5bddbabc986c50531223
Sébastien Berger <berger.sebastien@bluewin.ch>
2020-04-18 11:18:44 +0200

    Spotify + Modification de la base de donnée

---
+++ LoveMirorring/src/Api/Controllers/MatchingController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchingController : ControllerBase
+    {
+        private readonly LoveMirroringContext _context;
+        private IConfiguration Configuration { get; set; }
+
+        public MatchingController(LoveMirroringContext context, IConfiguration configuration)
+        {
+            _context = context;
+            Configuration = configuration;
+        }
+
+        [Route("sex")]
+        [HttpGet]
+        public IActionResult GetSex()
+        {
+            List<Sex> responses = _context.Sexes.ToList();
+            return new JsonResult(responses);
+        }
+
+        [Route("corpulences")]
+        [HttpGet]
+        public IActionResult GetCorpulences()
+        {
+            List<Corpulence> responses = _context.Corpulences.ToList();
+            return new JsonResult(responses);
+        }
+
+        [Route("religions")]
+        [HttpGet]
+        public IActionResult GetReligion()
+        {
+            List<Religion> responses = _context.Religions.ToList();
+            return new JsonResult(responses);
+        }
+
+        [Route("profils")]
+        [HttpGet]
+        public IActionResult GetProfils()
+        {
+            List<Profil> responses = _context.Profils.ToList();
+            return new JsonResult(responses);
+        }
+
+        [Route("SaveProfil")]
+        [HttpPost]
+        public IActionResult SaveProfil(UserChoiceViewModel userChoice)
+        {
+            Preference p = new Preference();
+            p.AgeMax = (short)userChoice.Age;
+            p.AgeMin = 18;
+            Religion religion = _context.Religions.Where(b => b.ReligionName == userChoice.Religion).FirstOrDefault();
+            AspNetUser user = _context.AspNetUsers.Where(b => b.UserName == userChoice.UserName).FirstOrDefault();
+            p.Id = user.Id;
+            p.SexualityId = 1;
+            _context.Preferences.Add(p);
+
+            try
+            {
+                _context.SaveChanges();
+                PreferenceReligion prefReligion = new PreferenceReligion();
+                prefReligion.ReligionId = religion.ReligionId;
+                prefReligion.PreferenceId = p.PreferenceId;
+                p.PreferenceReligions.Add(prefReligion);
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ LoveMirorring/src/mvc/Controllers/MatchingController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using mvc.Models;
+using mvc.ViewModels;
+using Newtonsoft.Json;
+
+namespace mvc.Controllers
+{
+    public class MatchingController : Controller
+    {
+        private HttpClient client = new HttpClient();
+        private IConfiguration Configuration { get; }
+
+        public MatchingController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> MatchingAsync()
+        {
+            string accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            string sexes = await client.GetStringAsync(Configuration["URLAPI"] + "api/Matching/Sex");
+            string religions = await client.GetStringAsync(Configuration["URLAPI"] + "api/Matching/religions");
+            string corpulences = await client.GetStringAsync(Configuration["URLAPI"] + "api/Matching/corpulences");
+            string profils = await client.GetStringAsync(Configuration["URLAPI"] + "api/Matching/profils");
+            List<Sex> resultSexes = JsonConvert.DeserializeObject<List<Sex>>(sexes);
+            List<Religion> resultReligions = JsonConvert.DeserializeObject<List<Religion>>(religions);
+            List<Corpulence> resultCorpulences = JsonConvert.DeserializeObject<List<Corpulence>>(corpulences);
+            List<Profil> resultProfils = JsonConvert.DeserializeObject<List<Profil>>(profils);
+            //List<Question> questionList = new List<Question>();
+            ViewData["sexes"] = resultSexes;
+            ViewData["religions"] = resultReligions;
+            ViewData["corpulences"] = resultCorpulences;
+            ViewData["profils"] = resultProfils;
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> SaveProfil(UserChoiceViewModel userChoice)
+        {
+            string accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            client.BaseAddress = new Uri(Configuration["URLAPI"] + "api/Matching/SaveProfil");
+            string json = await Task.Run(() => JsonConvert.SerializeObject(userChoice));
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = client.PostAsync(client.BaseAddress, httpContent);
+            var responseString = response.Result;
+            return View("Search");
+
+        }
+    }
+}

9ee84f0a3f5ac09dc737ef549dd6fe167884a10d
Tim <tim.allemann@outlook.com>
2020-06-04 17:32:10 +0200

    Filtre sur le chat général

--- LoveMirorring/src/mvc/Hubs/ChatHub.cs
+++ LoveMirorring/src/mvc/Hubs/ChatHub.cs
@@ -7,18 +7,42 @@
  */
 
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using mvc.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace mvc.Hubs
 {
     public class ChatHub : Hub
     {
+        private IConfiguration _configuration { get; set; }
+        public ChatHub(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            // Préparation de l'appel à l'API
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer");
+
+            // Récurération des données et convertion des données dans le bon type
+            string content = await client.GetStringAsync(_configuration["URLAPI"] + "api/Insults");
+            List<Insult> insults = JsonConvert.DeserializeObject<List<Insult>>(content);
+
+            List<string> words = insults.Select(i => i.InsultName).ToList();
+
+            ProfanityFilter.ProfanityFilter filter = new ProfanityFilter.ProfanityFilter();
+            filter.AddProfanity(words);
+            string censored = filter.CensorString(message);
+
+            await Clients.All.SendAsync("ReceiveMessage", user, censored);
         }
     }
 }

6348e3cdaa53e3a3790542b6f2de287c0ffc4d2a
Hans Morsch <kinghsm32@hotmail.com>
2020-06-08 16:26:11 +0200

    debug PAUL

--- LoveMirorring/src/mvc/Controllers/UserContactRequestsController.cs
+++ LoveMirorring/src/mvc/Controllers/UserContactRequestsController.cs
@@ -9,7 +9,9 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Unosquare.Swan;
 
 namespace mvc.Controllers
@@ -18,7 +20,6 @@ namespace mvc.Controllers
     {
         private readonly IConfiguration _configuration;
 
-        public object JsonConvert { get; private set; }
 
         public UserContactRequestsController(IConfiguration configuration)
         {
